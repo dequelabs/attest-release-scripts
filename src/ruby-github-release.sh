18d2a2fc4f309398ecec5c62d55bc58fcae31d16
Stephen Mathieson <me@stephenmathieson.com>
2019-03-01 14:44:20 -0500

    review updates

--- src/ruby-github-release.sh
+++ src/ruby-github-release.sh
@@ -9,14 +9,14 @@ throw() {
 
 # Ensure required env vars are set.
 [ -z "$GITHUB_TOKEN" ] && throw "GITHUB_TOKEN not set"
-# [ -z "$CIRCLE_PROJECT_REPONAME" ] && throw "CIRCLE_PROJECT_REPONAME not set"
-# [ -z "$CIRCLE_PROJECT_USERNAME" ] && throw "CIRCLE_PROJECT_USERNAME not set"
+[ -z "$CIRCLE_PROJECT_REPONAME" ] && throw "CIRCLE_PROJECT_REPONAME not set"
+[ -z "$CIRCLE_PROJECT_USERNAME" ] && throw "CIRCLE_PROJECT_USERNAME not set"
 
 # Ensure https://github.com/aktau/github-release is installed.
 command -v github-release > /dev/null 2>&1 || throw "Unable to locate github-release binary"
 
 # Ensure a single .gemspec file is present.
-GEMSPEC_COUNT=$(find . -type f -name "*.gemspec" | wc -l)
+GEMSPEC_COUNT=$(find . -type f -name "*.gemspec" | wc -l | tr -d [:space:])
 [ "$GEMSPEC_COUNT" != "1" ] && throw "Expecting a single .gemspec file"
 
 # Find .gemspec file.
