6baff4f839e71d9f3f5adeff46c2b5bea7862fb2
michael-siek <michaelsiek094@gmail.com>
2019-10-24 16:57:30 -0400

    [WIP] feat: Include changelog content into automated Github release

--- src/java-github-release.sh
+++ src/java-github-release.sh
@@ -29,13 +29,13 @@ get_changelog () {
 }
 
 # Ensure required env vars are set.
-# [ -z "$GITHUB_TOKEN" ] && throw "GITHUB_TOKEN not set"
-# [ -z "$CIRCLE_PROJECT_REPONAME" ] && throw "CIRCLE_PROJECT_REPONAME not set"
-# [ -z "$CIRCLE_PROJECT_USERNAME" ] && throw "CIRCLE_PROJECT_USERNAME not set"
+[ -z "$GITHUB_TOKEN" ] && throw "GITHUB_TOKEN not set"
+[ -z "$CIRCLE_PROJECT_REPONAME" ] && throw "CIRCLE_PROJECT_REPONAME not set"
+[ -z "$CIRCLE_PROJECT_USERNAME" ] && throw "CIRCLE_PROJECT_USERNAME not set"
 
 # # Ensure https://github.com/aktau/github-release is installed.
 # # NOTE: we install it from gopkg (gopkg.in/aktau/github-release.v0), so the binary has a `.v0` suffix.
-# command -v github-release.v0 > /dev/null 2>&1 || throw "Unable to locate github-release binary"
+command -v github-release.v0 > /dev/null 2>&1 || throw "Unable to locate github-release binary"
 
 # Parse the version number.
 PKG_VERSION=
@@ -55,9 +55,9 @@ done < pom.xml
 echo "Releasing v$PKG_VERSION"
 
 # Create a release.
-echo github-release.v0 release \
+github-release.v0 release \
   --user "$CIRCLE_PROJECT_USERNAME" \
   --repo "$CIRCLE_PROJECT_REPONAME" \
   --tag "v$PKG_VERSION" \
   --name "Release $PKG_VERSION" \
-  --description "\'$(get_changelog)\'"
+  --description "$(get_changelog)"
